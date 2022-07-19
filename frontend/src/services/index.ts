dd4bca43c4b41cdb14ef1e0d8479ca922c335cc5
linbrittany <brlin@itba.edu.ar>
2022-07-19 20:44:56 -0300

    feat: edit link

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,13 +1,16 @@
 import { AuthService } from "./authService";
 import { AxiosService } from "./axiosService";
+import { LinkService } from "./linkService";
 import { UserService } from "./userService";
 
 const authService = new AuthService();
 const userService = new UserService();
 const axiosService = new AxiosService();
+const linkService = new LinkService();
 
 export {
   authService,
   userService,
-  axiosService
+  axiosService,
+  linkService
 };
